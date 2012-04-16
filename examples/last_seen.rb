7829b6ba5c3e149944f73b1dbbbd14a19a95b78a
Anton Maminov <anton.linux@gmail.com>
2012-04-16 14:26:32 +0300

    remove all friends you've never seen before

--- examples/last_seen.rb
+++ examples/last_seen.rb
@@ -28,11 +28,15 @@ if __FILE__ == $0
   # sort an array of hashes by a value in the hash
   sorted_friends = friends.sort_by {|k| k['last_seen']['time']}
 
-  #sorted_friends[0...20].each do |f|
   sorted_friends.each do |f|
+    # Remove all friends you've never seen before
+    #if f['last_seen']['time'] == 0
+    #  resp = vk.api.friends_delete(uid: f['uid'])
+    #  puts "Successfully remove #{f['first_name']} #{f['last_name']}" if resp == 1
+    #end
+
     last_seen = Time.at(f['last_seen']['time'])
-    #puts "#{last_seen}: #{f['first_name']} #{f['last_name']}" unless f['online'] == 1
-    puts "#{last_seen}: #{f['first_name']} #{f['last_name']}"
+    puts "#{last_seen}: [#{f['uid']}] #{f['first_name']} #{f['last_name']}"
   end
 
 end
