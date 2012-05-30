ecb81940d099384af589bc2de6addb23ae665bd1
Anton Maminov <anton.linux@gmail.com>
2012-05-30 22:39:09 +0300

    Fix for temporarily suspended accounts

--- examples/last_seen.rb
+++ examples/last_seen.rb
@@ -22,20 +22,13 @@ if __FILE__ == $0
   vk.login!(email, pass)
 
   # http://vkontakte.ru/developers.php?o=-1&p=friends.get
-  tb = Time.now
   friends = vk.api.friends_get(fields: 'online,last_seen')
 
   # sort an array of hashes by a value in the hash
-  sorted_friends = friends.sort_by {|k| k['last_seen']['time']}
+  sorted_friends = friends.sort_by { |k| k['last_seen'] ? k['last_seen']['time'] : 0 }
 
   sorted_friends.each do |f|
-    # Remove all friends you've never seen before
-    #if f['last_seen']['time'] == 0
-    #  resp = vk.api.friends_delete(uid: f['uid'])
-    #  puts "Successfully remove #{f['first_name']} #{f['last_name']}" if resp == 1
-    #end
-
-    last_seen = Time.at(f['last_seen']['time'])
+    last_seen = f['last_seen'] ? Time.at(f['last_seen']['time']) : 'Temporarily suspended'
     puts "#{last_seen}: [#{f['uid']}] #{f['first_name']} #{f['last_name']}"
   end
 
