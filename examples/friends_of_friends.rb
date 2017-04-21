2d010dee5727100f74b35e62a1ff5d821a81198f
Anton Maminov <anton.maminov@gmail.com>
2017-04-21 18:43:53 +0300

    update friends_of_friends example

--- examples/friends_of_friends.rb
+++ examples/friends_of_friends.rb
@@ -21,7 +21,7 @@ if __FILE__ == $0
   second_circle = {}
   second_circle.default = 0
   # [uid, uid, ...]
-  my_friends = vk.api.friends_get['items']
+  my_friends = vk.api.friends_get(order: 'hints')['items']
   good_friends = 0
   bad_friends = 0
 
@@ -41,12 +41,18 @@ if __FILE__ == $0
   puts "\nComplete"
 
   # Отбросим друзей и людей, у которых только один общий знакомый
-  second_circle.reject! {|uid, count| my_friends.include?(uid) || current_user['id'] == uid || count < 2}
+  second_circle.reject! { |uid, count| my_friends.include?(uid) || current_user['id'] == uid || count < 2 }
 
   puts "Total people in 2nd circle: #{second_circle.size}"
 
   # Сортировка по количеству общих знакомых
-  sorted_second_circle = second_circle.sort{|a, b| b[1]<=>a[1]} # <-- Hash sorting by value
+  sorted_second_circle = second_circle.sort { |a, b| b[1] <=> a[1] } # <-- Hash sorting by value
+
+  # информацию о отправленных заявках на добавление в друзья
+  friends_requests = vk.api.friends_getRequests(out: 1)['items']
+
+  # Отбросим людей которым уже послали приглашение в друзья
+  sorted_second_circle.reject! { |arry| friends_requests.include?(arry[0]) }
 
   sorted_second_circle = sorted_second_circle[0...19]
 
@@ -56,8 +62,19 @@ if __FILE__ == $0
   common_friends = vk.api.users_get(:user_ids => "#{sorted_second_circle.transpose[0].join(',')}")
 
   sorted_second_circle.each do |uid, count|
-    f = common_friends.find{|f| f['id'] == uid} # <-- array of hashes: finding hash with value for key
-    puts "[#{count}]: [#{f['id']}] #{f['first_name']} #{f['last_name']}"
+    begin
+      user = vk.api.users_get(user_id: uid).first
+
+      unless user['deactivated']
+        f = common_friends.find { |f| f['id'] == uid }
+        puts "[#{count}]: [#{f['id']}] #{f['first_name']} #{f['last_name']}"
+        vk.api.friends_add(user_id: f['id'])
+      end
+    rescue Vkontakte::API::Error => err
+      puts err.message
+      sleep 60
+      retry
+    end
   end
 
 end
